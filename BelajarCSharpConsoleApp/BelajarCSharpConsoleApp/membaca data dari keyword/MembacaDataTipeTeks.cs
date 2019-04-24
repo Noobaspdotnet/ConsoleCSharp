5fb0dc9f01667ea2660e8067cb98314d5812bdfb
Noobaspdotnet <noobaspdotnet30@gmail.com>
2019-04-24 22:12:58 +0700

    cara membaca Data tipe Teks

---
+++ BelajarCSharpConsoleApp/BelajarCSharpConsoleApp/membaca data dari keyword/MembacaDataTipeTeks.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BelajarCSharpConsoleApp.membaca_data_dari_keyword
+{
+    class MembacaDataTipeTeks
+    {
+        static void Main(string[] args)
+        {
+            string nama;
+
+            Console.Write("masukan nama anda :");
+            nama = Console.ReadLine();
+
+            Console.WriteLine("Hallo " + nama + ", selamat datang di tutorial pemrogaman C#");
+
+            Console.ReadLine();
+        }
+    }
+}
