4b0f76b58697ee7aa936bff819cb1f8054f3458e
Noobaspdotnet <noobaspdotnet30@gmail.com>
2019-04-24 22:31:08 +0700

    cara membaca data tipe karakter

---
+++ BelajarCSharpConsoleApp/BelajarCSharpConsoleApp/membaca data dari keyword/MembacaDataTipeKarakter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BelajarCSharpConsoleApp.membaca_data_dari_keyword
+{
+    class MembacaDataTipeKarakter
+    {
+        static void main()
+        {
+            char c;
+            string s;
+            Console.Write("Masukan karakter :");
+            s = Console.ReadLine();
+
+            //konversi tipe string ke karakter
+            if (char.TryParse(s, out c))
+            {
+                Console.WriteLine("karakter yang di masukan adalah : " + c + "'");
+
+            }
+            else
+            {
+                Console.WriteLine("data yang dimasukan " + "tidak dapat sikonversi" + "ke tipe karakter");
+;            }
+        }
+    }
+}
