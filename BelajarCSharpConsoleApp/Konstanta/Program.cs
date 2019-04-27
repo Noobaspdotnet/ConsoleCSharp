925b936db4f507e3b94bcdf51242d2a1c52f4339
Noobaspdotnet <noobaspdotnet30@gmail.com>
2019-04-27 21:43:28 +0700

    mengenal konstanta

---
+++ BelajarCSharpConsoleApp/Konstanta/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Konstanta
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string Produk = "Belajar ASP.net";
+
+            Console.WriteLine("anda sedang belajar : " + Produk);
+            Console.ReadLine();
+        }
+    }
+}
